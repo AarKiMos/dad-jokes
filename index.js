b372ebdf5caf3d99405805ea1917cf1a3dbfa108
Aachman Mittal <m.aachman@gmail.com>
2024-04-10 16:18:24 +0530

    Create express server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+import server from "./src/server.js";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
